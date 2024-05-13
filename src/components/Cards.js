67f7a75704926ba3e097a71dcd22a3a2bc4be269
Basmahx <138460878+Basmahx@users.noreply.github.com>
2024-05-13 15:22:33 +0200

    home page presque fini

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React from "react"; // necessary for writing React components.
 import "../styles/cards.css";
+// The NavLink component wraps the entire content of the card and acts as a clickable link to navigate to a specific route.
+import { NavLink } from "react-router-dom";
 
-const Cards = () => {
+
+const Cards = ({ logement }) => {
     return (
-        <div>
-            
-        </div>
-    )
+       <NavLink className="fiche"
+        to={`/fichelogement/${logement.id}`}
+        key={logement.id}>
+
+            <div className="card">
+                    <div className="cardsContainer" style={{
+                        backgroundImage:`linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%), url(${logement.cover})`,
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center',
+                        }}>
+                        <h3>{logement.title}</h3>
+                    </div> 
+            </div>    
+       </NavLink>
+    );
 };
+
+export default Cards;

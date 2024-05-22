380e08809a19ae51b5a0d1e1c48c4e4a9852f235
Basmahx <138460878+Basmahx@users.noreply.github.com>
2024-05-22 19:13:44 +0200

    almost done with the About page

---
+++ src/components/Collapse.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "../styles/collapse.css";
+import flecheBas from "../assets/fleche-bas.png";
+import flecheHaut from "../assets/fleche-haut.png";
+
+const Collapse = ({ title, description }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="valeurs">
+            <button
+                className="collapse-title"
+                style={{ border: 'none', outline: 'none' }}
+                onClick={toggleOpen}
+                aria-expanded={isOpen}
+                aria-controls={`collapse-content-${title}`}
+            >
+                {title}
+                {isOpen ? (
+                    <img src={flecheHaut} alt="up-arrow" />
+                ) : (
+                    <img src={flecheBas} alt="down-arrow" />
+                )}
+            </button>
+            {isOpen && (
+                <div id={`collapse-content-${title}`} className="collapse-paragraph">
+                    {description}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Collapse;
